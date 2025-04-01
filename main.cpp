9cca7eeb6ba1388aa734f771e661ff4e7b70afe6
FrozenNeverSleep <108141789+FrozenNeverSleep@users.noreply.github.com>
2025-03-31 21:40:08 -0400

    s

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-#include "raylib.h"
+int main() {
+    InitWindow(32, 32, "Pixel Game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
